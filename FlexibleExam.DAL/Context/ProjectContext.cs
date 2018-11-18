4e67f92814e0201bc804c9253ec355cf07e0925f
Gökhan ÖZGÜR <g.ozgur@outlook.com>
2018-11-19 01:14:59 +0300

    DAL

---
+++ FlexibleExam.DAL/Context/ProjectContext.cs
@@ -0,0 +1,101 @@
+﻿using FlexibleExam.Core.Entity;
+using FlexibleExam.Core.Enum;
+using FlexibleExam.Model;
+using FlexibleExam.Model.Option;
+using FlexibleExam.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexibleExam.DAL.Context
+{
+    public class ProjectContext : DbContext
+    {
+
+        public ProjectContext()
+        {
+            Database.Connection.ConnectionString = "Server=.;Database=FlexibleExam;uid=sa;pwd=123";
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<AppUser> AppUsers { get; set; }
+
+        public DbSet<AssignedCourse> AssignedCourses { get; set; }
+
+        public DbSet<AssignedExam> AssignedExams { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<CourseUnitOrSubject> CourseUnitOrSubjects { get; set; }
+
+        public DbSet<Exam> Exams { get; set; }
+
+        public DbSet<ExamAnswerMarking> ExamAnswerMarkings { get; set; }
+
+        public DbSet<ExamQuestion> ExamQuestions { get; set; }
+
+        public DbSet<ExamQuestionAnswerOption> ExamQuestionAnswerOptions { get; set; }
+
+        public DbSet<ExamQuestionDistribution> ExamQuestionDistributions { get; set; }
+
+        public DbSet<ExamResult> ExamResults { get; set; }
+
+
+        public override int SaveChanges()
+        {
+
+            var modifiedEntries = ChangeTracker.Entries().Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
+
+            string identity = WindowsIdentity.GetCurrent().Name;
+            string computerName = Environment.MachineName;
+            DateTime dateTime = DateTime.Now;
+            string getIp = RemoteIP.IpAddress;
+
+
+            foreach (var item in modifiedEntries)
+            {
+                CoreEntity entity = item.Entity as CoreEntity;
+
+                if (item != null)
+                {
+                    if (item.State == EntityState.Added)
+                    {
+                        entity.Status = Status.Active;
+                        entity.CreatedUserName = identity;
+                        entity.CreatedComputerName = computerName;
+                        entity.CreatedDate = dateTime;
+                        entity.CreatedIP = getIp;
+                    }
+                    else if (item.State == EntityState.Modified)
+                    {
+                        //entity.Status = Status.Updated;
+                        entity.ModifiedUserName = identity;
+                        entity.ModifiedComputerName = computerName;
+                        entity.ModifiedDate = dateTime;
+                        entity.ModifiedIP = getIp;
+                    }
+
+                }
+            }
+
+
+            return base.SaveChanges();
+        }
+
+    }
+}
--- FlexibleExam.Model/Option/AppUser.cs
+++ FlexibleExam.Model/Option/AppUser.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FlexibleExam.Model
+namespace FlexibleExam.Model.Option
 {
     public enum Role
     {
